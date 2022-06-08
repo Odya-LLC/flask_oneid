7b0b9586232597c7b52fed6161e7fb9fe17b54e7
Odilxon <mmuhtor@gmail.com>
2022-06-08 15:43:55 +0500

    readme fix

--- setup.py
+++ setup.py
@@ -6,16 +6,20 @@ This is the description for that library
 """
 from setuptools import setup
 
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
 
 setup(
     name='Flask-OneID',
-    version='1.0.0',
+    version='1.0.1',
     url='https://github.com/Odya-LLC/flask_oneid',
     license='MIT',
     author='odya',
     author_email='support@odya.uz',
     description='OneID integration with Flask application, (only for Uzbekistan)',
-    long_description=__doc__,
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     packages=['flask_oneid'],
     zip_safe=False,
     include_package_data=True,
